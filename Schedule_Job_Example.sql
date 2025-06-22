eb9dbd8de7b26c9d352c48a9799ba66389be49e8
kelvin Ayogu <Kelvinayogu39@gmail.com>
2025-06-22 21:11:40 +0100

    Update Schedule_Job_Example.sql

--- Schedule_Job_Example.sql
+++ Schedule_Job_Example.sql
@@ -1,4 +1,10 @@
 -- This file provides an example of scheduling logic
 -- Use SQL Server Agent to run this command every hour:
 
+```sql
+    IF EXISTS (SELECT 1 FROM msdb.dbo.sysjobs WHERE name = N'Batch_Update_Procedure_Job')
+BEGIN
+    EXEC msdb.dbo.sp_delete_job @job_name = N'Batch_Update_Procedure_Job';
+END
+```
 -- EXEC [dbo].[UpdateBatchStatus] @BatchNumber = 1, @Status = 'In Progress';
